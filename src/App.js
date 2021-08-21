4121593b20b8fa132a4c29508720f21eddcea4ab
prince Cados <princecadosnew@gmail.com>
2021-08-21 12:00:52 +0430

    create calculator

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React,{ useState} from 'react';
+import logo from './logo.png';
+import './App.css';
+
+console.log(logo);
+const App = () => {
+    const [result, setResult] = useState("");
+    const handleClick = (e) => {
+        setResult(result.concat(e.target.name));
+    }
+    const clear = () => {
+        setResult("");
+    }
+    const backspace = () => {
+        setResult(result.slice(0, -1));
+    }
+    const calculate = () => {
+        try {
+            setResult(eval(result).toString());
+        } catch (err) {
+            setResult("Error")
+        }
+    }
+    return (
+        <div className="calculator">
+            <form>
+                <img src={logo} className="calc-logo" alt="Logo" />
+                <input className="display-screen" type="text" value={result} />
+            </form>
+
+            <div className="keypad">
+                <button onClick={clear} id="clear">Clear</button>
+                <button onClick={backspace} id="backspace" className="symbol">C</button>
+                <button name="/" onClick={handleClick} className="symbol">&divide;</button>
+                <button name="7" onClick={handleClick}>7</button>
+                <button name="8" onClick={handleClick}>8</button>
+                <button name="9" onClick={handleClick}>9</button>
+                <button name="*" onClick={handleClick} className="symbol">&times;</button>
+                <button name="4" onClick={handleClick}>4</button>
+                <button name="5" onClick={handleClick}>5</button>
+                <button name="6" onClick={handleClick}>6</button>
+                <button name="-" onClick={handleClick} className="symbol">&ndash;</button>
+                <button name="1" onClick={handleClick}>1</button>
+                <button name="2" onClick={handleClick}>2</button>
+                <button name="3" onClick={handleClick}>3</button>
+                <button name="+" onClick={handleClick} className="symbol">+</button>
+                <button name="0" onClick={handleClick}>0</button>
+                <button name="." onClick={handleClick}>.</button>
+                <button onClick={calculate} id="result">=</button>
+            </div>
+        </div>
+
+    );
+}
+
+export default App;
